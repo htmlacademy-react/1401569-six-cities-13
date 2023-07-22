ed541ed948bb7b7cf358e5b01938a5cd77145cbb
Alexey Slyusarenko <slyusarenko.a.n@gmail.com>
2023-07-22 10:45:09 +0300

    Добавлена маршрутизация

--- src/component/app/app.tsx
+++ src/component/app/app.tsx
@@ -1,11 +1,42 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites/favorites';
+import OfferPage from '../../pages/offers-page/offers-page';
+import NotFoundPage from '../../pages/error-page/error-page';
 
 type AppProps = {
   countCitiesCard: number;
 };
 
 function App({ countCitiesCard }: AppProps): JSX.Element {
-  return <MainPage countCitiesCard={countCitiesCard} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path = {AppRoute.Main}
+          element = {<MainPage countCitiesCard = {countCitiesCard} />}
+        />
+        <Route
+          path = {AppRoute.Login}
+          element = {<LoginPage />}
+        />
+        <Route
+          path = {AppRoute.Favorites}
+          element = {<FavoritesPage />}
+        />
+        <Route
+          path = {AppRoute.Offer}
+          element = {<OfferPage />}
+        />
+        <Route
+          path = '*'
+          element = {<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,10 @@
 export const Mocks = {
   CountCitiesCard: 3,
 };
+
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id',
+}
